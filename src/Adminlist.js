02f264c1d212ceb5b0c64539e6054046b39c1eba
karanawate <karanawate8@gmail.com>
2021-09-12 13:49:17 +0530

    Increment number useState using setdata(data + 1)

--- src/Adminlist.js
+++ src/Adminlist.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import View from './View';
 const Adminlist = () => {
 	const [count, setCount] = useState(0);
-	const [data, setData] = useState(100);
+	const [data, setData] = useState(0);
 
 	return <div>
 		<p>Hello admin</p>{count}
+		<p>Number increment {data}</p>{count}
 		<button onClick={ () => setCount(count + 1)}>click</button>
+		<button onClick={ () =>setData(data + 1)}>onclick increment</button>
 		<View data={data} />
 	</div>
 }
