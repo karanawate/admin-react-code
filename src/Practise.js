dc42cd9b1cd5b1adb227f99e96496a94a0d133b3
karanawate <karanawate8@gmail.com>
2022-07-16 22:33:34 +0530

    useEffect using [user,customer] we can pass

--- src/Practise.js
+++ src/Practise.js
@@ -4,14 +4,13 @@ const Practise = () =>{
     const [users, setUsers] = useState(0);
 
     useEffect(() =>{
-        console.log("useeffec working: dsfsdf");
-    });
+        console.warn("useeffec working: dsfsdf", users);
+    },[])
 
     return(
         <div>
-        <h5>useEffect when load the page</h5>
-        <h1>default state value is {users}</h1>
-        <button onClick={() =>setUsers(users + 5)}>click me</button>
+        <h5>useEffect hooke {users}</h5>
+        <button onClick={() =>setUsers(users + 1)}>click me</button>
     </div>
     );
 };
