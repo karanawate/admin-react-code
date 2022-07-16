79e2c75a739df7ced0c68d469238bd1b0910b7ef
karanawate <karanawate8@gmail.com>
2022-07-16 22:20:03 +0530

    useEffect using..

--- src/Practise.js
+++ src/Practise.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Practise = () =>{
     const [users, setUsers] = useState(0);
+
+    useEffect(() =>{
+        console.log("useeffec working: dsfsdf");
+    });
+
     return(
         <div>
-        <h1>useState have two property first is default as 0  users & second is property is updating setUsers</h1>
+        <h5>useEffect when load the page</h5>
         <h1>default state value is {users}</h1>
         <button onClick={() =>setUsers(users + 5)}>click me</button>
     </div>
